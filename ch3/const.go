77ec2d0839a84f9ee9886b427dbb13b6f5be1514
zhaoj62 <13928441612@163.com>
2018-01-31 23:06:43 +0800

    ch3

---
+++ ch3/const.go
@@ -0,0 +1,11 @@
+// const 
+package main
+
+import "fmt"
+
+func main() {
+	s := "hello"
+	fmt.Printf("%T\n", s)
+	a := s[-1]
+	b := 1/0
+}
---
+++ ch3/utf8.go
@@ -0,0 +1,8 @@
+// utf8 
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("hello, 世界"))
+}
